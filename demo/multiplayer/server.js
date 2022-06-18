5c3f4e69dea95141ae6083f23cda8b4bec0d1e8e
Chewbacca132 <krystof.bresky@zaci.gymnazium-opatov.cz>
2022-06-18 21:23:33 +0200

    Generalized static path on the server

--- demo/multiplayer/server.js
+++ demo/multiplayer/server.js
@@ -4,7 +4,8 @@ const script = require("./script");
 const {PlayerSoldier, AISoldier} = script;
 const IP = process.argv[2] || "localhost"//require("os").networkInterfaces()["Wi-Fi"][0].address;
 const PORT = 80;
-const server = new GameServer("C:/Users/chewb/node/gameEngine/demo/multiplayer/client.html", 20, 60, "../..");
+console.log(__dirname);
+const server = new GameServer(__dirname + "/client.html", 20, 60, __dirname + "/../../");
 server.server.listen(PORT, IP, () => console.log(`running on ${IP}:${PORT}`))
 server.onConnection = function(player){
     new PlayerSoldier(new engine.Vector(0, 0), new engine.Vector(1, 1), player);
