cd4ad5f0ff10edbf82d09f3e1c4a91fa1a18ef72
Chewbacca132 <krystof.bresky@zaci.gymnazium-opatov.cz>
2022-08-28 08:29:09 +0200

    move source code files inside src folder

--- engine/engine.js
+++
@@ -1,6 +0,0 @@
-module.exports.Vector = require("./vector");
-module.exports.Game = require("./game");
-module.exports.GameObject = require("./gameObject");
-module.exports.math = require("./math");
-module.exports.physics = require("./physics");
-module.exports.random = require("./random");
